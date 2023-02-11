6e7b8665f448cb95ca12946012f4bf3fa9fbd6d8
Brandon <bbkx226@gmail.com>
2023-02-11 14:15:16 +0800

    Frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import { logo } from './assets';
+import { Home, CreatePost } from './page';
+
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]">
+      <Link to="/">
+        <img src={logo} alt="logo" className="w-32 object-contain" />
+      </Link>
+
+      <Link to="/create-post" className="font-inter hover:bg-black  font-medium bg-[#333333] text-white px-4 py-2 rounded-md">Create</Link>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+  </BrowserRouter>
+);
+
+export default App;
