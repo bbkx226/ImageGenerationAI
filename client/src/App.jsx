43b3d6bd81f36990d52e1a7466283f320ab39e77
Brandon <bbkx226@gmail.com>
2023-02-11 17:21:40 +0800

    Frontend Ver 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,9 @@ import { Home, CreatePost } from './page';
 const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]">
-      <Link to="/">
-        <img src={logo} alt="logo" className="w-32 object-contain" />
-      </Link>
-
-      <Link to="/create-post" className="font-inter hover:bg-black  font-medium bg-[#333333] text-white px-4 py-2 rounded-md">Create</Link>
+      <a href='https://www.bbkx.live'><img src={logo} alt="logo" className="w-32 object-contain" /></a>
+      <Link to="/" className="font-inter hover:bg-black  font-medium bg-[#333333] text-white px-4 py-2 rounded-md ml-auto">Home</Link>
+      <Link to="/create-post" className="font-inter hover:bg-black  font-medium bg-[#333333] text-white px-4 py-2 rounded-md ml-2">Create</Link>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
